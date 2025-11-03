1e0d883c394387a9542d0713c09557d4f73d9312
Ilyeschrif22 <ilyes.chrif@esprit.tn>
2025-11-03 10:27:37 +0100

    first commit

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// server.js
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -8,14 +7,11 @@ const emailRoutes = require("./routes/emailRoutes");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use("/api", emailRoutes);
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
